d77b613bda510aba56e55d7290f63d7f6ba56686
Vivek <vivektheintel@gmail.com>
2024-09-12 23:11:30 -0400

    Incorporating addition of SELL blocks to DataList

--- src/main/java/com/vv/personal/twm/portfolio/model/DataList.java
+++ src/main/java/com/vv/personal/twm/portfolio/model/DataList.java
@@ -25,12 +25,49 @@ public class DataList {
     DataNode dataNode = new DataNode(instrument);
     if (head == null) {
       head = dataNode;
+      tail = dataNode;
     } else {
-      dataNode.setPrev(tail);
-      tail.setNext(dataNode);
+      if (instrument.getDirection() == MarketDataProto.Direction.BUY) {
+        dataNode.setPrev(tail);
+        tail.setNext(dataNode);
+        tail = dataNode;
+      } else { // in case of SELL, need to find the correct place for new node according to trade
+        // date
+        boolean insertedSellNode = false;
+        DataNode last = tail;
+        while (last != null) { // as the first node is assumed to be of BUY type, ALWAYS.
+          // keep going left until a date lower than sell is found and insert after that
+
+          if (isCorrectPlaceForSell(instrument, last.getInstrument())) {
+            dataNode.setNext(last.getNext());
+            dataNode.setPrev(last);
+            last.getNext().setPrev(dataNode);
+            last.setNext(dataNode);
+            insertedSellNode = true;
+            break;
+          }
+
+          last = last.getPrev();
+        }
+
+        if (!insertedSellNode) {
+          throw new UnsupportedOperationException(
+              "Does not allow short selling of instruments yet!");
+        }
+      }
     }
-    tail = dataNode;
-    dataNode.computeAcb(); // computing acb once linking is done
+
+    // dataNode.computeAcb(); // removing from here due to possible middle placement of sell blocks
+    // causing lot of compute overhead
+  }
+
+  /** Invoke at end of construction of DataList, in order to compute ACB for all nodes of list */
+  public void computeAcb() {
+    DataNode current = head;
+    while (current != null) {
+      current.computeAcb();
+      current = current.getNext();
+    }
   }
 
   public void display() {
@@ -39,5 +76,15 @@ public class DataList {
       log.info("{}", current);
       current = current.getNext();
     }
+  }
+
+  private boolean isCorrectPlaceForSell(
+      MarketDataProto.Instrument sellInstrument, MarketDataProto.Instrument instrument) {
+    return sellInstrument.getTicker().getData(0).getDate()
+        > instrument.getTicker().getData(0).getDate();
+  }
+
+  DataNode getHead() { // only allowed for testing
+    return head;
   }
 }
--- src/test/java/com/vv/personal/twm/portfolio/model/DataListTest.java
+++ src/test/java/com/vv/personal/twm/portfolio/model/DataListTest.java
@@ -1,5 +1,7 @@
 package com.vv.personal.twm.portfolio.model;
 
+import static com.vv.personal.twm.artifactory.generated.equitiesMarket.MarketDataProto.Direction.BUY;
+import static com.vv.personal.twm.artifactory.generated.equitiesMarket.MarketDataProto.Direction.SELL;
 import static com.vv.personal.twm.portfolio.TestConstants.PRECISION;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,50 +16,51 @@ class DataListTest {
 
   @Test
   public void dataListCreation() {
-    MarketDataProto.Instrument firstImnt =
-        generateInstrument(10, 5.1, MarketDataProto.Direction.BUY);
-    MarketDataProto.Instrument secondImnt =
-        generateInstrument(20, 10, MarketDataProto.Direction.BUY);
+    MarketDataProto.Instrument firstImnt = generateInstrument(10, 5.1, BUY, 20240909);
+    MarketDataProto.Instrument secondImnt = generateInstrument(20, 10, BUY, 20240912);
     MarketDataProto.Instrument thirdImnt =
-        generateInstrument(5, 12, MarketDataProto.Direction.SELL);
-    MarketDataProto.Instrument fourthImnt =
-        generateInstrument(10, 15, MarketDataProto.Direction.BUY);
+        generateInstrument(5, 12, MarketDataProto.Direction.SELL, 20240913);
+    MarketDataProto.Instrument fourthImnt = generateInstrument(10, 15, BUY, 20240920);
 
     DataList dataList = new DataList();
     dataList.addBlock(firstImnt);
     dataList.addBlock(secondImnt);
-    dataList.addBlock(thirdImnt);
     dataList.addBlock(fourthImnt);
+    dataList.addBlock(thirdImnt);
+    dataList.computeAcb();
 
     dataList.display();
 
     assertEquals(10, dataList.getHead().getRunningQuantity(), PRECISION);
     assertEquals(51, dataList.getHead().getAcb().getTotalAcb(), PRECISION);
     assertEquals(5.1, dataList.getHead().getAcb().getAcbPerShare(), PRECISION);
+    assertEquals(BUY, dataList.getHead().getInstrument().getDirection());
 
     assertEquals(30, dataList.getHead().getNext().getRunningQuantity(), PRECISION);
     assertEquals(251, dataList.getHead().getNext().getAcb().getTotalAcb(), PRECISION);
     assertEquals(8.3666666, dataList.getHead().getNext().getAcb().getAcbPerShare(), PRECISION);
+    assertEquals(BUY, dataList.getHead().getNext().getInstrument().getDirection());
 
     assertEquals(25, dataList.getTail().getPrev().getRunningQuantity(), PRECISION);
     assertEquals(191, dataList.getTail().getPrev().getAcb().getTotalAcb(), PRECISION);
     assertEquals(7.64, dataList.getTail().getPrev().getAcb().getAcbPerShare(), PRECISION);
+    assertEquals(SELL, dataList.getTail().getPrev().getInstrument().getDirection());
 
     assertEquals(35, dataList.getTail().getRunningQuantity(), PRECISION);
     assertEquals(341, dataList.getTail().getAcb().getTotalAcb(), PRECISION);
     assertEquals(9.742857143, dataList.getTail().getAcb().getAcbPerShare(), PRECISION);
+    assertEquals(BUY, dataList.getTail().getInstrument().getDirection());
   }
 
   private MarketDataProto.Instrument generateInstrument(
-      double qty, double price, MarketDataProto.Direction direction) {
+      double qty, double price, MarketDataProto.Direction direction, int date) {
     return MarketDataProto.Instrument.newBuilder()
         .setQty(qty)
         .setDirection(direction)
         .setTicker(
             MarketDataProto.Ticker.newBuilder()
                 .setName("test")
-                .addData(
-                    MarketDataProto.Value.newBuilder().setDate(20240911).setPrice(price).build())
+                .addData(MarketDataProto.Value.newBuilder().setDate(date).setPrice(price).build())
                 .build())
         .build();
   }
