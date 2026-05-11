ed2eb312474e72c40d9f30eef9e0f859763d5519
Vivek <vivektheintel@gmail.com>
2026-05-10 22:06:13 -0400

    Switch to j21 and agnostic symlink in startup script

--- bin/twm-portfolio.sh
+++ bin/twm-portfolio.sh
@@ -5,7 +5,7 @@ BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in 
 cd $BIN_PATH/../target/
 JAR_NAME=`ls *jar`
 JAR_PATH=$BIN_PATH/../target/$JAR_NAME
-JAVA_PATH=$HOME/.jdks/jdk17/bin/java
+JAVA_PATH=$HOME/.jdks/jdk-current/bin/java
 
 echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
 $JAVA_PATH $JAVA_PARAM -jar $JAR_PATH
