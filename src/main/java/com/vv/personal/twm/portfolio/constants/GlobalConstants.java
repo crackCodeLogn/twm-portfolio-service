fbaf1ec9c86656c669cf36a74f4d9fd0e78e20da
Vivek <vivektheintel@gmail.com>
2026-07-05 18:55:25 -0400

    Switch CALC_PYTHON_FEIGN_NAME to updated eureka discovery name

--- src/main/java/com/vv/personal/twm/portfolio/constants/GlobalConstants.java
+++ src/main/java/com/vv/personal/twm/portfolio/constants/GlobalConstants.java
@@ -6,7 +6,7 @@ package com.vv.personal.twm.portfolio.constants;
  */
 public final class GlobalConstants {
   public static final String BANK_CRDB_SERVICE_FEIGN_NAME = "twm-bank-crdb-service";
-  public static final String CALC_PYTHON_FEIGN_NAME = "twm-calc-py-service";
+  public static final String CALC_PYTHON_FEIGN_NAME = "twm-calc-py-engine";
   public static final String CALC_SERVICE_FEIGN_NAME = "twm-calc-service";
   public static final String MARKET_DATA_CRDB_SERVICE_FEIGN_NAME = "twm-market-data-crdb-service";
   public static final String MARKET_DATA_PYTHON_ENGINE_FEIGN_NAME = "twm-market-data-engine";
